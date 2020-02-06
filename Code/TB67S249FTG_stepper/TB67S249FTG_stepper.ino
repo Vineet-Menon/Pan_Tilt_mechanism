1eac9b8625d7a4224a5226248c9477b93b6fbdb7
VinMen98 <vineetpvmenon@gmail.com>
2020-02-07 03:29:29 +0530

    Stepper code changed

--- Code/TB67S249FTG_stepper/TB67S249FTG_stepper.ino
+++ Code/TB67S249FTG_stepper/TB67S249FTG_stepper.ino
@@ -1,40 +1,49 @@
 /*
  * Author: Vineet V. Menon
- * Started on: 23/02/2019
+ * Started on: 06/02/2020
  * Components: Arduino Uno/Mega, Toshiba TB67S249FTG stepper motor driver   
- * Power supply voltage: 24V 
+ * Power supply voltage: 12V 
  * Resolution of stepper motor= 1.8 degree per pulse
  * 
  * 
  */
-const int spr= 400;        // Micro-stepping is set to 2
-const int dirp=4;          // dir+ is given to GPIO pin 4 
-const int pulp=5;         // pul+ is given to GPIO pin 5
+const int dir= 8;          // Direction (CW/CCW) is given to GPIO pin 6 
+const int clk= 7;          // Step (clk) is given to GPIO pin 5
+//const int ena= 4;          // Enable pin is connected to GPIO pin 4
+const int dm_0= 11;        // Dmode0 pin is connected to GPIO pin 11
+const int dm_1= 10;        // Dmode1 pin is connected to GPIO pin 10
+const int dm_2= 9;         // Dmode2 pin is connected to GPIO pin 9
 
 void setup() 
 {
-  pinMode(dirp,OUTPUT);    // Set dir+ pin as output
-  pinMode(pulp,OUTPUT);    // Set pul+ pin as output
+  pinMode(dir,OUTPUT);    // Set dir pin as output
+  pinMode(clk,OUTPUT);    // Set step pin as output
+//  pinMode(ena,OUTPUT);    // Set enable pin as output
+  pinMode(dm_0,OUTPUT);    // Set Dmode0 pin as output
+  pinMode(dm_1,OUTPUT);    // Set Dmode1 pin as output
+  pinMode(dm_2,OUTPUT);    // Set Dmode2 pin as output
   Serial.begin(9600);      // Serial communication baud rate set to 9600
+//  digitalWrite(ena,HIGH);  // Set Enable pin as high to activate the driver
+  digitalWrite(dm_0,HIGH);
+  digitalWrite(dm_1,LOW);
+  digitalWrite(dm_2,LOW); // Set microstepping factor by setting the states of Dmode0,Dmode1 and Dmode2
 }
 
-void clockwise(float deg)   // function to rotate the kinect sensor clockwise by a certain degree 'deg'
+void clockwise(float deg)   // function to rotate the motor clockwise by a certain degree 'deg'
 {
   int i,freq_pulse;
   float theta= 0;
-  freq_pulse = (4*deg)/0.9;   // driven pulley would rotate 1/4 times the degree rotated by the driver pulley
+  freq_pulse = (deg)/0.9;   // driven pulley would rotate 1/4 times the degree rotated by the driver pulley
   Serial.print("Clockwise ");
   Serial.println(deg,2);
-  digitalWrite(dirp,HIGH);
+//  digitalWrite(dir,HIGH);
   for (i=1; i<=freq_pulse; i++)   //fuction to generate a square wave of period 2t    
   {
-     digitalWrite(pulp,HIGH);
-//     delayMicroseconds(16000);     // the largest value that will produce an accurate delay is 16383
-     delay(20);                      // if required delay is above 16 milli seconds
-     digitalWrite(pulp,LOW);
-//     delayMicroseconds(16000);
-     delay(20);
-     theta= theta+ (0.9/4);       // angle increments by 0.9/4 everytime this for loop ends
+     digitalWrite(clk,HIGH);
+     delayMicroseconds(250);     // the largest value that will produce an accurate delay is 16383
+     digitalWrite(clk,LOW);
+     delayMicroseconds(50);
+     theta= theta + (0.9);       // angle increments by 1.8/4 everytime this for loop ends
      Serial.println(theta);
   }
 }
@@ -43,19 +52,17 @@ void counterclockwise(float deg)   // function to rotate the kinect sensor anti-
 {
   int j, freq_pulse;
   float theta= 0;
-  freq_pulse = (4*deg)/0.9;     // driven pulley would rotate 1/4 times the degree rotated by the driver pulley
+  freq_pulse = (deg)/0.9;     // driven pulley would rotate 1/4 times the degree rotated by the driver pulley
   Serial.print("Anti-clockwise ");  
   Serial.println(deg,2);
-  digitalWrite(dirp,LOW);
+  digitalWrite(dir,LOW);
   for (j=1; j<=freq_pulse; j++)     // function to generate a square wave of rate 2t
   {
-     digitalWrite(pulp,HIGH);
-//     delayMicroseconds(16000);
-     delay(20);
-     digitalWrite(pulp,LOW);
-//     delayMicroseconds(16000);
-     delay(20);
-     theta= theta+ (0.9/4);         // angle increments by 0.9/4 everytime this for loop ends
+     digitalWrite(clk,HIGH);
+     delayMicroseconds(250);
+     digitalWrite(clk,LOW);
+     delayMicroseconds(50);
+     theta= theta+ (0.9);         // angle increments by 0.9/4 everytime this for loop ends
      Serial.println(theta);
   }
 }
@@ -63,16 +70,15 @@ void counterclockwise(float deg)   // function to rotate the kinect sensor anti-
 void loop() 
 {
   int k;
-  for (k=1; k<=3; k++)
+  for (k=1; k<=10; k++)
   {
-    delay(2000);
+    delay(3000);
     Serial.print("Round ");
     Serial.println(k);       // print the round number on the Serial monitor
-    clockwise(90);           // call clockwise function to rotate by 45 degrees 
+    clockwise(360);           // call clockwise function to rotate by 45 degrees 
     delay(2000);
-    counterclockwise(100);    // call counterclockwise function to rotate by 45 degrees
-    delay(2000);
-    clockwise(40);
+//    counterclockwise(360);    // call counterclockwise function to rotate by 45 degrees
+//    delay(2000);
   }
   delay(30000);              // wait for 30 seconds
 }
